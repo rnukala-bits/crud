550ceb16b189a63764e86cc3e3f31e835c477096
rnukala-divantech <rnukalabits@gmail.com>
2023-06-24 12:09:12 -0700

    updated dbserver

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 		// Define database connection variables
-		$servername = "DBServer";
-		$username = "DB_USER";
-		$password = "DB_PASSWORD";
-		$dbname = "DB_NAME";
+		$servername = "strangler.mysql.database.azure.com";
+		$username = "root_admin";
+		$password = "@Gdc$hht%1";
+		$dbname = "strangler";
 
 		// Create database connection
 		$conn = new mysqli($servername, $username, $password, $dbname);

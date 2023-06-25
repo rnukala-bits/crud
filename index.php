83f5e2ff588a1f293cf0faf74e14787a29ad3a99
rnukala-divantech <rnukalabits@gmail.com>
2023-06-25 12:35:13 -0700

    test php rendering

--- index.php
+++ index.php
@@ -5,41 +5,15 @@
 </head>
 <body>
 	<h1>MySQL Table Viewer</h1>
-	<?php
-		// Define database connection variables
-		$servername = "strangler.mysql.database.azure.com";
-		$username = "root_admin";
-		$password = "@Gdc$hht%1";
-		$dbname = "strangler";
-
-		// Create database connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		printf("Reading data from table: \n");
-		// Query database for all rows in the table
-		$sql = "SELECT * FROM strangler.employees";
-		$result = $conn->query($sql);
-		printf("Successfully read from table: \n");
-
-		if ($result->num_rows > 0) {
-			printf("Inside the loop: \n");
-			// Display table headers
-			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-			// Loop through results and display each row in the table
-			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-
-		// Close database connection
-		$conn->close();
-	?>
+	<div class="center-align">
+	<ul>
+		<div class="left-align">
+			<li><a href=""><strong>View Catalog</strong></a></li>
+			<li><a href=""><strong>Add a Product</strong></a></li>
+			<li><a href=""><strong>Update a Product</strong></a></li>
+			<li><a href=""><strong>Remove a Product</strong></a></li>
+		</div>
+	</ul>
+</div>
 </body>
 </html>

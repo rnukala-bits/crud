46b00356f6e20d1279323a43e493fafe842ad6dc
rnukala-divantech <rnukalabits@gmail.com>
2023-06-28 21:40:08 -0700

    override and bring mysql back

--- index.php
+++ index.php
@@ -16,20 +16,17 @@
 	$link = mysqli_connect($servername, $username, $password) or die("Unable to Connect to '$servername'");
 	mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
-	$test_query = "SHOW TABLES FROM $dbname";
-	$result = mysqli_query($link, $test_query);
+	$test_query = "select * from departments";
+	
+	$result = $link -> query($sql);
 
-	$tblCnt = 0;
-	while($tbl = mysqli_fetch_array($result)) {
-	$tblCnt++;
-	#echo $tbl[0]."<br />\n";
-	}
+	// Fetch all
+	$result -> fetch_all(MYSQLI_ASSOC);
 
-	if (!$tblCnt) {
-	echo "There are no tables<br />\n";
-	} else {
-	echo "There are $tblCnt tables<br />\n";
-	} 
+	// Free result set
+	$result -> free_result();
+
+	$link -> close(); 
 ?>
 </body>
 </html>
